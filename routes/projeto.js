473b3ec126e02646d7a0449048322b6febf6754b
Aron <aronkerk8@gmail.com>
2023-05-19 12:28:11 -0300

    Improving route projeto

--- routes/projeto.js
+++ routes/projeto.js
@@ -75,4 +75,114 @@ router.post('/getone', (req, res, next) => {
     })
 })
 
+router.post('/getonetarefa', (req, res, next) => {
+    const idprojeto = req.body.idprojeto;
+    let andamento = [];
+    let tarefa = [];
+    let concluido = [];
+
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'SELECT * FROM tarefa WHERE projeto_idprojeto = ?',
+            idprojeto,
+            (error, resultado, field) => {
+                conn.release();
+
+                if(error){
+                    res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+
+                resultado.map((e) =>{
+                    if(e.status === "Em andamento"){
+                        andamento = [...andamento, {
+                            idtarefa: e.idtarefa,
+                            nome: e.nome,
+                            data: e.data,
+                            status: e.status,
+                            observacao: e.observacao,
+                            projeto_idprojeto: e.projeto_idprojeto
+                        }]
+                    }
+
+                    if(e.status === "Tarefa"){
+                        tarefa = [...tarefa, {
+                            idtarefa: e.idtarefa,
+                            nome: e.nome,
+                            data: e.data,
+                            status: e.status,
+                            observacao: e.observacao,
+                            projeto_idprojeto: e.projeto_idprojeto
+                        }]
+                    }
+
+                    if(e.status === "Concluido"){
+                        concluido = [...concluido, {
+                            idtarefa: e.idtarefa,
+                            nome: e.nome,
+                            data: e.data,
+                            status: e.status,
+                            observacao: e.observacao,
+                            projeto_idprojeto: e.projeto_idprojeto
+                        }]
+                    }
+                })
+
+                res.send({tarefa: tarefa, andamento: andamento, concluido: concluido})
+            }
+        )
+    })
+})
+
+router.post('/tarefa', (req, res, next) => {
+    let idTarefa;
+    const idprojeto = req.body.idprojeto;
+    const nome = req.body.nome;
+    const date = req.body.date;
+    const checklist = req.body.checklist;
+    const status = req.body.status;
+
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'INSERT INTO tarefa (nome, status, data, projeto_idprojeto) VALUES (?, ?, ?, ?) ',
+            [nome, status, date, idprojeto],
+            (error, resultado, field) => {
+                conn.release();
+
+                if(error){
+                    res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                idTarefa = resultado?.insertId;
+
+                mysql.getConnection((error, conn) => {
+                    checklist.map((e) => {
+                        conn.query(
+                            'INSERT INTO checklist (nome, status, tarefa_idtarefa) VALUES (?, false, ?)',
+                            [e.nome, idTarefa],
+                            (error, resultado, field) => {
+                                conn.release();
+
+                                if(error){
+                                    res.status(500).send({
+                                        error: error,
+                                        response: null
+                                    });
+                                }
+                            }
+                        )
+                    }) 
+                })   
+                
+                res.send("Tarefa criado com sucesso!");
+                       
+            }
+        )
+    })
+})
+
 module.exports = router;
