92cf7015e18ebb9dcbe0c60594fdde76f319de56
Aron Felippe Kerkhoven <43013959+Nirayuki@users.noreply.github.com>
2023-05-24 20:56:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,22 @@ const cors = require("cors");
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-
-
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header(
-//         'Access-Control-Allow-Header',
-//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//     );
-
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//         return res.status(200).send({});
-//     }
-//     next();
-// });
-
 
 app.get('/', (req, res) => {
     res.send("Caminho encontrado");
