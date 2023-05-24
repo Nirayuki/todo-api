a886852f8b477d94af8b8f66d3ae53c91c91c8c3
Aron <aronkerk8@gmail.com>
2023-05-24 18:22:30 -0300

    TESTE

--- app.js
+++ app.js
@@ -4,30 +4,24 @@ const bodyParser = require('body-parser');
 require('dotenv').config()
 const cors = require("cors");
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,
-   optionSuccessStatus:200,
-}
-
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-app.use(cors(corsOptions))
+app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//         'Access-Control-Allow-Header',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-});
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//         return res.status(200).send({});
+//     }
+//     next();
+// });
 
 
 app.get('/', (req, res) => {
